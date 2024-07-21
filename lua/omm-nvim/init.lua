f4778f2b3f5d22260177b40699750e534fa917b9
davitostes <davisiqueira591@gmail.com>
2024-07-21 04:30:08 -0300

    feat: mapping Enter to quit the response to omm exit

--- lua/omm-nvim/init.lua
+++ lua/omm-nvim/init.lua
@@ -9,6 +9,12 @@ function M.init()
 	vim.cmd("term omm")
 	-- Switch to terminal mode
 	vim.cmd("startinsert")
+	-- Map q to close the window in terminal mode
+	vim.api.nvim_buf_set_keymap(0, "t", "q", "<CR>", { noremap = true, silent = true })
+	-- Map Q to close the window in terminal mode
+	vim.api.nvim_buf_set_keymap(0, "t", "Q", "<CR>", { noremap = true, silent = true })
+	-- Map ctrl+c to close the window in terminal mode
+	vim.api.nvim_buf_set_keymap(0, "t", "<C-q>", "<CR>", { noremap = true, silent = true })
 end
 
 return M
