2ff402d8e2c72cd35701e2e0674079021a11b516
davitostes <davisiqueira591@gmail.com>
2024-07-21 04:26:59 -0300

    fix: removing keymaps

--- lua/omm-nvim/init.lua
+++ lua/omm-nvim/init.lua
@@ -9,12 +9,6 @@ function M.init()
 	vim.cmd("term omm")
 	-- Switch to terminal mode
 	vim.cmd("startinsert")
-	-- Map q to close the window in terminal mode
-	vim.api.nvim_buf_set_keymap(0, "t", "q", "<C-\\><C-n>BufferDelete<CR>", { noremap = true, silent = true })
-	-- Map Q to close the window in terminal mode
-	vim.api.nvim_buf_set_keymap(0, "t", "Q", "<C-\\><C-n>BufferDelete<CR>", { noremap = true, silent = true })
-	-- Map ctrl+c to close the window in terminal mode
-	vim.api.nvim_buf_set_keymap(0, "t", "<C-q>", "<C-\\><C-n>BufferDelete<CR>", { noremap = true, silent = true })
 end
 
 return M
