c250412c5f78200e3c43b84317d4b470e91be425
davitostes <davisiqueira591@gmail.com>
2024-07-21 03:25:04 -0300

    feat: closing window after pressing q

--- lua/omm-nvim/init.lua
+++ lua/omm-nvim/init.lua
@@ -1,10 +1,16 @@
 local M = {}
 
 function M.init()
+	-- Open a new window at the bottom
 	vim.cmd("botright new")
+	-- Resize the window
 	vim.cmd("resize 20")
-  vim.fn.termopen("omm")
-  vim.cmd("startinsert")
+	-- Open the terminal and runs omm
+	vim.fn.termopen("omm")
+	-- Switch to terminal mode
+	vim.cmd("startinsert")
+	-- Map q to close the window in terminal mode
+	vim.api.nvim_buf_set_keymap(0, "t", "q", "<C-\\><C-n>:q<CR>", { noremap = true, silent = true })
 end
 
 return M
